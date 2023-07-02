863d0042279366a3942b475869370f512b064ce8
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-07-02 17:45:53 -0600

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { List, Item, MovieLink } from './EditorList.styled';
+import { fetchTrending } from '../../FetchApi/Api';
+import Loader from 'components/Loader/Loader';
 
+export const EditorList = ({ films }) => {
+  const location = useLocation();
 
-const HomePage = () => {
+  return (
+    <List>
+      {films.map(film => (
+        <Item key={film.id}>
+          <MovieLink to={`/movies/${film.id}`} state={{ from: location }}>
+            {film.title}
+          </MovieLink>
+        </Item>
+      ))}
+    </List>
+  );
+};
+
+EditorList.propTypes = {
+  films: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export const HomePage = () => {
     return (
       <>
       </>
     );
 }
-
-export default HomePage;
