01405624b6c57577efcbb82f213967fe58300ab9
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-07-04 23:12:15 +0000

    Deploying to gh-pages from @ prozhyvarov/goit-react-hw-05-movies@9bedaf54389689116e144fb3676ff0838bce43e4 🚀

---
+++ static/js/673.1f368021.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[673],{2673:function(t,s,e){e.r(s);var n=e(2791),i=e(7689),o=e(9014),r=e(184),u=(0,n.lazy)((function(){return e.e(9).then(e.bind(e,9))}));s.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)(o.x7,{position:"top-right",toastOptions:{duration:1500}}),(0,r.jsxs)(n.Suspense,{children:[(0,r.jsx)(u,{}),(0,r.jsx)(i.j3,{})]})]})}}}]);
+//# sourceMappingURL=673.1f368021.chunk.js.map
--- static/js/673.fb4e4946.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[673],{673:function(t,s,e){e.r(s);var n=e(791),i=e(689),o=e(73),r=e(184),u=(0,n.lazy)((function(){return e.e(9).then(e.bind(e,9))}));s.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)(o.x7,{position:"top-right",toastOptions:{duration:1500}}),(0,r.jsxs)(n.Suspense,{children:[(0,r.jsx)(u,{}),(0,r.jsx)(i.j3,{})]})]})}}}]);
-//# sourceMappingURL=673.fb4e4946.chunk.js.map
